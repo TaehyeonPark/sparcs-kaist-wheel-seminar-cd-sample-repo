b8afa2714500b7fdfdf2e891ea9e442330882ba7
TaehyeonPark <devtaehyeon@gmail.com>
2023-08-03 23:09:15 +0900

    Github Action assigment.

--- test/app.test.js
+++ test/app.test.js
@@ -24,6 +24,26 @@ describe('GET /', () => {
 // });
 
 // IN-LAB TODO: test script for /health-check
+describe('GET /health-check', () => {
+  it('should return 200 OK', () => {
+    return request(app).get('/health-check')
+      .expect(200);
+  });
 
+  it('should return "ok"', () => {
+    return request(app).get('/health-check')
+      .expect('ok');
+  });
+
+  it('should return 404 (must fail)', () => {
+    return request(app).get('/health-check')
+      .expect(404);
+  });
+
+  it('should return "ok" (must fail)', () => {
+    return request(app).get('/health-check')
+      .expect('not ok');
+  });
+});
 
 // TODO : test script for /assignment
